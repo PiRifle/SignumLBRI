8a7b9038be9978d81da8cfd9bb25275c0f6149ef
PiRifle <piotrekpolocz@gmail.com>
2022-09-07 22:52:31 +0200

    corrected lint, updated bootstrap, ui

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import compression from "compression";  // compresses requests
+import compression from "compression"; // compresses requests
 import session from "express-session";
 import bodyParser from "body-parser";
 import lusca from "lusca";
@@ -9,15 +9,15 @@ import path from "path";
 import mongoose from "mongoose";
 import passport from "passport";
 import bluebird from "bluebird";
-import {showPDF} from "./util/generatePDF"
+import { showPDF } from "./util/generatePDF";
 import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
 
 // Controllers (route handlers)
 import * as homeController from "./controllers/home";
 import * as userController from "./controllers/user";
-import * as apiController from "./controllers/api";
-import * as contactController from "./controllers/contact";
-import * as bookController from "./controllers/book"
+// import * as apiController from "./controllers/api";
+// import * as contactController from "./controllers/contact";
+import * as bookController from "./controllers/book";
 
 // API keys and Passport configuration
 import * as passportConfig from "./config/passport";
@@ -29,12 +29,21 @@ const app = express();
 const mongoUrl = MONGODB_URI;
 mongoose.Promise = bluebird;
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
-    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
-).catch(err => {
-    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+  })
+  .catch((err) => {
+    console.log(
+      `MongoDB connection error. Please make sure MongoDB is running. ${err}`
+    );
     // process.exit();
-});
+  });
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
@@ -43,43 +52,46 @@ app.set("view engine", "pug");
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
+app.use(
+  session({
     resave: true,
     saveUninitialized: true,
     secret: SESSION_SECRET,
     store: new MongoStore({
-        mongoUrl,
-        mongoOptions: {
-            autoReconnect: true
-        }
-    })
-}));
+      mongoUrl,
+      mongoOptions: {
+        autoReconnect: true,
+      },
+    }),
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
 app.use((req, res, next) => {
-    res.locals.user = req.user;
-    next();
+  res.locals.user = req.user;
+  next();
 });
 app.use((req, res, next) => {
-    // After successful login, redirect back to the intended page
-    if (!req.user &&
+  // After successful login, redirect back to the intended page
+  if (
+    !req.user &&
     req.path !== "/login" &&
     req.path !== "/adduser" &&
     !req.path.match(/^\/auth/) &&
-    !req.path.match(/\./)) {
-        req.session.returnTo = req.path;
-    } else if (req.user &&
-    req.path == "/account") {
-        req.session.returnTo = req.path;
-    }
-    next();
+    !req.path.match(/\./)
+  ) {
+    req.session.returnTo = req.path;
+  } else if (req.user && req.path == "/account") {
+    req.session.returnTo = req.path;
+  }
+  next();
 });
 
 app.use(
-    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+  express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
 );
 
 /**
@@ -87,24 +99,37 @@ app.use(
  */
 // app.get("/contact", contactController.getContact);
 // app.post("/contact", contactController.postContact);
-app.get("/",passportConfig.isAuthenticated, homeController.index);
+app.get("/", passportConfig.isAuthenticated, homeController.index);
 app.get("/login", userController.getLogin);
 app.post("/login", userController.postLogin);
-app.get("/logout",passportConfig.isAuthenticated, userController.logout);
+app.get("/logout", passportConfig.isAuthenticated, userController.logout);
 app.get("/forgot", userController.getForgot);
 app.post("/forgot", userController.postForgot);
 app.get("/reset/:token", userController.getReset);
 app.post("/reset/:token", userController.postReset);
-app.get("/verify/:token", userController.getVerify)
+app.get("/verify/:token", userController.getVerify);
+app.get("/resendverify", userController.getResendVerify);
 app.get("/setup", userController.getSetUp);
 app.post("/setup", userController.postSignup);
 app.get("/signup", userController.getSignup);
 app.post("/signup", userController.postSignup);
 app.get("/account", passportConfig.isAuthenticated, userController.getAccount);
-app.post("/account/profile", passportConfig.isAuthenticated, userController.postUpdateProfile);
-app.post("/account/password", passportConfig.isAuthenticated, userController.postUpdatePassword);
-app.post("/account/delete", passportConfig.isAuthenticated, userController.postDeleteAccount);
-app.get("/account/unlink/:provider", passportConfig.isAuthenticated, userController.getOauthUnlink);
+app.post(
+  "/account/profile",
+  passportConfig.isAuthenticated,
+  userController.postUpdateProfile
+);
+app.post(
+  "/account/password",
+  passportConfig.isAuthenticated,
+  userController.postUpdatePassword
+);
+app.post(
+  "/account/delete",
+  passportConfig.isAuthenticated,
+  userController.postDeleteAccount
+);
+app.get("/account/unlink/:provider", passportConfig.isAuthenticated);
 
 /**
  * API examples routes.
@@ -120,15 +145,34 @@ app.get("/account/unlink/:provider", passportConfig.isAuthenticated, userControl
 //     res.redirect(req.session.returnTo || "/");
 // });
 
-app.get("/find", passportConfig.isAuthenticated, passportConfig.isSeller, bookController.getFindListing)
-app.get("/book/registry", passportConfig.isAuthenticated, bookController.getBookRegistry);
-app.get("/book/list", passportConfig.isAuthenticated, bookController.getBooks);
-app.get("/book/sell", passportConfig.isAuthenticated, bookController.getSellBook)
-app.post("/book/sell", passportConfig.isAuthenticated, passportConfig.isSeller, bookController.postSellBook)
-app.get("/book/fromisbn", passportConfig.isAuthenticated, bookController.getFillBookData)
-app.get("/book/:itemID", passportConfig.isAuthenticated, bookController.editBook)
-app.post("/book/:itemID/sell", passportConfig.isAuthenticated, passportConfig.isSeller, bookController.sellBook);
+// app.get("/find", passportConfig.isAuthenticated, passportConfig.isSeller, bookController.getFindListing);
+// app.get("/book/registry", passportConfig.isAuthenticated, bookController.getBookRegistry);
+// app.get("/book/list", passportConfig.isAuthenticated, bookController.getBooks);
+// app.get("/book/sell", passportConfig.isAuthenticated, bookController.getSellBook);
+// app.post("/book/sell", passportConfig.isAuthenticated, passportConfig.isSeller, bookController.postSellBook);
+// app.get("/book/:itemID", passportConfig.isAuthenticated, bookController.editBook);
+// app.post("/book/:itemID/sell", passportConfig.isAuthenticated, passportConfig.isSeller, bookController.sellBook);
 
+app.get(
+  "/book/add",
+  passportConfig.isAuthenticated,
+  bookController.getSellBook
+);
+app.post(
+  "/book/add",
+  passportConfig.isAuthenticated,
+  bookController.postSellBook
+);
+app.get(
+  "/book/fromisbn",
+  passportConfig.isAuthenticated,
+  bookController.getFillBookData
+);
+app.get(
+    "/label",
+    passportConfig.isAuthenticated,
+    bookController.getPrintLabel
+);
 
 // const applicationRoutes = express.Router()
 // applicationRoutes.post("/login", userController.postLoginApp)
@@ -138,10 +182,10 @@ app.post("/book/:itemID/sell", passportConfig.isAuthenticated, passportConfig.is
 // applicationRoutes.post("/sell", passportConfig.isAuthenticatedApp, passportConfig.isSeller, bookController.postSellBookApp)
 // applicationRoutes.get("/find", passportConfig.isAuthenticatedApp, passportConfig.isSeller, bookController.getFindListingApp)
 // applicationRoutes.post("/:itemID/sell", passportConfig.isAuthenticatedApp, passportConfig.isSeller, bookController.sellBookApp);
-    
+
 // app.use("/app", applicationRoutes)
 
-app.get("/print", showPDF)
+app.get("/print", showPDF);
 app.get("/print/fetch", showPDF);
 
 export default app;
